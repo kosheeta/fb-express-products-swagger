b21a0dda200fb2310603453ad5ef3b1dd1401181
ZagorodnikhNick <nick2112@mail.ru>
2026-02-19 10:23:09 +0300

    Update starter

--- frontend/src/api/productsApi.js
+++ frontend/src/api/productsApi.js
@@ -7,21 +7,21 @@ import { api } from "./apiClient";
  */
 
 export async function getProducts() {
-  // return (await api.get("/products")).data;
+    // return (await api.get
   throw new Error("TODO: реализуйте getProducts()");
 }
 
 export async function createProduct(payload) {
-  // return (await api.post("/products", payload)).data;
+  // return (await api.post
   throw new Error("TODO: реализуйте createProduct(payload)");
 }
 
 export async function updateProduct(id, patch) {
-  // return (await api.patch(`/products/${id}`, patch)).data;
+  // return (await api.patch
   throw new Error("TODO: реализуйте updateProduct(id, patch)");
 }
 
 export async function deleteProduct(id) {
-  // return (await api.delete(`/products/${id}`)).data;
+  // return (await api.delete
   throw new Error("TODO: реализуйте deleteProduct(id)");
 }
